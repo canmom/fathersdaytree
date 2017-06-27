3d171c67adf57614ae6c4179ef093dbdf3bde9cc
canmom <bryn.dickinson@gmail.com>
2017-06-27 08:47:33 +0100

    Enable minification for production
    Webpack's default minifier UglifyJS does not support ES6 features such
    as classes, so Babel and Babili are added to the requirements and
    configured for use as a loader.
    
    The npm build command has some significance inside npm, so it is
    switched to npm pack.

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,18 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
 };
